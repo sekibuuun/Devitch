a424ccdad975677c91d5708304c76aafff10907b
sekibuuun <akaunto.no.001@gmail.com>
2024-07-03 15:48:03 +0900

    add: signin 画面

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,17 +1,20 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
 	return (
 		<div className="flex justify-between py-3 bg-teal-300">
-			<div className="pl-10 flex items-center">
-				<h1 className="text-white text-2xl font-bold">Header</h1>
-			</div>
+			<Link href="/" className="pl-10 flex items-center">
+				<h1 className="text-white text-2xl font-bold">Devitch</h1>
+			</Link>
 			<div className="pr-10">
-				<Avatar>
-					<AvatarImage />
-					<AvatarFallback>CN</AvatarFallback>
-				</Avatar>
+				<Link href="/signin">
+					<Avatar>
+						<AvatarImage />
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>{" "}
+				</Link>
 			</div>
 		</div>
 	);
