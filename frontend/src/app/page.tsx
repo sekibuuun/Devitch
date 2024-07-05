97664eb6e8333f676fee26a35fdce5efa403e2fd
sekibuuun <akaunto.no.001@gmail.com>
2024-07-05 20:50:52 +0900

    fix: hellohandlerの取得

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+
 export default function Home() {
-	async function getHello(): Promise<string> {
-		const res = await fetch("http://localhost:8080/hello");
-		return res.text();
-	}
-	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">{getHello()}</h1>
-		</div>
-	);
+  const [hello, setHello] = useState('Loading...');
+
+  useEffect(() => {
+    async function getHello() {
+      const res = await fetch("http://localhost:8080/hello");
+      const text = await res.text();
+      setHello(text);
+    }
+
+    getHello();
+  }, []); // 空の依存配列を渡すことで、コンポーネントのマウント時にのみ実行されます
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">{hello}</h1>
+    </div>
+  );
 }
